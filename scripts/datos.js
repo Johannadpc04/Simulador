bdcb93f10f3fd209d4daef3e1994e0dc3acc0b10
Johannadpc04 <johanna.dpc@gmail.com>
2024-08-30 17:16:17 -0500

    act

--- scripts/datos.js
+++ scripts/datos.js
@@ -4,31 +4,32 @@ rondas = [
 
     preguntas: [
       {
-        nombre: 'Menciona el día más festejado en méxico',
+        nombre: 'Sinonimo de inteligente',
         respuestas: [
-          {nombre: 'Día de las madres', numero: 39},
-          {nombre: 'Día de la Virgen de Guadalupe', numero: 25},
-          {nombre: 'Día de muertos', numero: 18},
-          {nombre: 'Día de la independencia', numero: 10},
-          {nombre: 'Día del niño', numero: 8}
+          {nombre: 'Lucido', numero: 1},
+          {nombre: 'Agudo', numero: 1},
+          {nombre: 'Perpicaz', numero: 1},
+          {nombre: 'Sagaz', numero: 1},
+          {nombre: 'Listo', numero: 1}
         ]
       },
       {
-        nombre: 'Parte del cuerpo en donde los dolores son más frecuentes',
+        nombre: 'Sinonimo de Bello',
         respuestas: [
-          {nombre: 'Cabeza', numero: 27},
-          {nombre: 'Estomago/vientre', numero: 25},
-          {nombre: 'Espalda', numero: 22},
-          {nombre: 'Piernas', numero: 19}
+          {nombre: 'Agraciado', numero: 1},
+          {nombre: 'Precioso', numero: 1},
+          {nombre: 'Atractivo', numero: 1},
+          {nombre: 'Guapo', numero: 1},
+          {nombre: 'Hermoso', numero: 1}
         ]
       },      
       {
-        nombre: 'Menciona algo que se le puede hacer al piso',
+        nombre: 'Sinonimo de valiente',
         respuestas: [
-          {nombre: 'Trapearlo', numero: 32},
-          {nombre: 'Barrerlo', numero: 29},
-          {nombre: 'Pulirlo', numero: 13},
-          {nombre: 'Pisarlo', numero: 7}         
+          {nombre: 'Valeroso', numero: 1},
+          {nombre: 'Bravo', numero: 1},
+          {nombre: 'Audaz', numero: 1},
+          {nombre: 'Osado', numero: 1}         
         ]
       }      
     ] 
@@ -39,31 +40,31 @@ rondas = [
   {
     preguntas: [
       {
-        nombre: 'Menciona los elementos que integran el escudo nacional',
+        nombre: 'Sinonimo de caminar',
         respuestas: [
-          {nombre: 'Águila', numero: 48},
-          {nombre: 'Serpiente', numero: 24},
-          {nombre: 'Nopal florecido', numero: 12},          
-          {nombre: 'Rama de laurel', numero: 9},
-          {nombre: 'Listón tricolor', numero: 7}       
+          {nombre: 'Marchar', numero: 1},
+          {nombre: 'Recorrer', numero: 1},
+          {nombre: 'Deambular', numero: 1},          
+          {nombre: 'Pasear', numero: 1},
+          {nombre: 'Andar', numero: 1}       
         ]
       },
       {
-        nombre: 'Problema que te puede pasar cuando estas en el baño',
+        nombre: 'Sinonimo de decir',
         respuestas: [
-          {nombre: 'Se acaba el papel', numero: 37},
-          {nombre: 'Estar estreñido', numero: 21},
-          {nombre: 'No hay agua', numero: 18},
-          {nombre: 'Esta tapado el escusado', numero: 14}          
+          {nombre: 'Articular', numero: 1},
+          {nombre: 'Afirmar' numero: 1},
+          {nombre: 'Declarar', numero: 1},
+          {nombre: 'Manifestar', numero: 1}          
         ]
       },
       {
-        nombre: 'Menciona algo opuesto a la libertad',
+        nombre: 'Sinonimo de pequeño',
         respuestas: [
-          {nombre: 'Esclavitud', numero: 39},
-          {nombre: 'Encierro', numero: 25},
-          {nombre: 'Prisión', numero: 18},
-          {nombre: 'Opresión', numero: 10}
+          {nombre: 'Compacto', numero: 1},
+          {nombre: 'Diminuto', numero: 1},
+          {nombre: 'Reducido', numero: 1},
+          {nombre: 'Chico', numero: 1}
         ]
       }
     ] 
@@ -74,31 +75,32 @@ rondas = [
   {
     preguntas: [
       {
-        nombre: 'Cuando le pides un aumento a tu jefe, dinos una cosa que te aseguras de mencionar',
+        nombre: 'Sinonimo de Fuerte',
         respuestas: [
-          {nombre: 'Hago bien mi trabajo', numero: 27},
-          {nombre: 'Costo de la vida', numero: 21},
-          {nombre: 'Me lo merezco', numero: 14},          
-          {nombre: 'Por favor', numero: 11},
-          {nombre: 'Ya tengo mucho tiempo trabajando', numero: 9}       
+          {nombre: 'Vigoros', numero: 1},
+          {nombre: 'Firme', numero: 1},
+          {nombre: 'Robusto', numero: 1},          
+          {nombre: 'Resistente', numero: 1},
+          {nombre: 'Potente', numero: 1}       
         ]
       },
       {
-        nombre: 'Menciona el tipo de torta más común',
+        nombre: 'Antonimo de alto',
         respuestas: [
-          {nombre: 'De jamón', numero: 34},
-          {nombre: 'Cubana', numero: 30},
-          {nombre: 'Cochinita', numero: 25},
-          {nombre: 'Ahogada', numero: 8}      
+          {nombre: 'Microscopico', numero: 1},
+          {nombre: 'Corto', numero: 1},
+          {nombre: 'Reducido', numero: 1},
+          {nombre: 'Bajo', numero: 1},      
+          {nombre: 'Pequeño', numero: 1}      
         ]
       },
       {
-        nombre: 'Menciona el taco más pedido en una taquería o puesto callejero',
+        nombre: 'Antonimo de fuerte',
         respuestas: [
-          {nombre: 'Asada', numero: 39},
-          {nombre: 'Al pastor', numero: 19},
-          {nombre: 'De tripa', numero: 13},
-          {nombre: 'Combinado', numero: 8}
+          {nombre: 'Flojo', numero: 1},
+          {nombre: 'Vulnerable', numero: 1},
+          {nombre: 'Endeble', numero: 1},
+          {nombre: 'Debil', numero: 1}
         ]
       }
     ] 
@@ -109,31 +111,31 @@ rondas = [
   {
     preguntas: [
       {
-        nombre: 'Dinos que es lo primero que harías si te ganaras la lotería',
+        nombre: 'Nombre un género literario',
         respuestas: [
-          {nombre: 'Pagar deudas', numero: 28},
-          {nombre: 'Viajar', numero: 23},
-          {nombre: 'Comprar una casa', numero: 17},          
-          {nombre: 'Hacer donaciones', numero: 12},
-          {nombre: 'Salirse de trabajar', numero: 10}       
+          {nombre: 'Narrativo', numero: 1},
+          {nombre: 'Lirico', numero: 1},
+          {nombre: 'Dramatico', numero: 1},          
+          {nombre: 'Epico', numero: 1},
+          {nombre: 'Didactic', numero: 1}       
         ]
       },
       {
-        nombre: 'Bebida que toman las personas mientras trabajan',
+        nombre: 'Mencione una figura literaria',
         respuestas: [
-          {nombre: 'Café', numero: 31},
-          {nombre: 'Agua', numero: 29},
-          {nombre: 'Té', numero: 19},
-          {nombre: 'Cola cola', numero: 17}      
+          {nombre: 'Metafora', numero: 1},
+          {nombre: 'Simil', numero: 1},
+          {nombre: 'Hiperbole', numero: 1},
+          {nombre: 'Personificacion', numero: 1}      
         ]
       },
       {
-        nombre: 'Menciona un medio de trasporte que se utiliza para llegar al trabajo',
+        nombre: 'Nombre un autor ecuatoriano',
         respuestas: [
-          {nombre: 'Automóvil', numero: 33},
-          {nombre: 'Camión', numero: 27},
-          {nombre: 'Raite', numero: 14},
-          {nombre: 'Bicicleta', numero: 3}
+          {nombre: 'Jorge Icaza', numero: 1},
+          {nombre: 'Juan Leon Mera', numero: 1},
+          {nombre: 'Joaquin Gallegos Lara', numero: 1},
+          {nombre: 'Eugenio Espejo', numero: 1}
         ]
       }
     ] 
